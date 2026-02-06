fdea91ae46d3e8b59e773d158c1b33b6f60f5b5c
Gourav554 <Jgourav554@gmail.com>
2026-02-06 14:05:49 +0530

    Change

---
+++ Questions.java/Pascal_Triangle.java
@@ -0,0 +1,15 @@
+public class Pascal_Triangle {
+    public static void main(String[] args) {
+        int n=5;
+        
+        for(int i=0;i<n;i++){
+            int value=1;
+            for(int j=0;j<=i;j++){
+                System.out.print(value+ "  " );
+                value = value * (i-j) / (j+1);
+               // System.out.print(value+ "  ");
+            }
+            System.out.println();
+        }
+    }
+}
