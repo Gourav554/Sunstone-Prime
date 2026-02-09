b501a019b3b19e96f58a2a73004646f656b56fe0
Gourav554 <Jgourav554@gmail.com>
2026-02-09 21:19:49 +0530

    array

--- ArrayQue.java/Q2.java
+++ ArrayQue.java/Q2.java
@@ -3,7 +3,7 @@ public class Q2
  {
     public static void main(String[] args) {
         int arr[]= {8,31,-21,4,33};
-         int sum=0;
+         int sum=0;   
         for(int i=0;i<arr.length;i++){
             sum =sum+arr[i];
         }
