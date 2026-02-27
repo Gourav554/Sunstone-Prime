4a00fab5276e15f553369e6dd61d05697b6a4680
Gourav554 <Jgourav554@gmail.com>
2026-02-27 20:00:55 +0530

    Sort

--- Topic.java/InsertionSort.java
+++ Topic.java/InsertionSort.java
@@ -4,19 +4,13 @@ public class InsertionSort {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
-        
         int n = sc.nextInt();
 
-       
         int[] arr = new int[n];
-
         
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
-    
         for (int i = 1; i < n; i++) {
 
             int key = arr[i];
@@ -29,12 +23,10 @@ public class InsertionSort {
 
             arr[j + 1] = key;
         }
-
         
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
-
         sc.close();
     }
 }
