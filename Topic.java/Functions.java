e8629d72150f73dd493f134735392600416ba647
Gourav554 <Jgourav554@gmail.com>
2026-03-01 22:47:51 +0530

    Function

--- Topic.java/Functions.java
+++ Topic.java/Functions.java
@@ -10,11 +10,10 @@ public class Functions {  // Calling ke liye
             }
         
     public static void main(String[]args) {
-                    Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
             int a = sc.nextInt();
              int b = sc.nextInt();
        System.out.println(Gcd(a, b));
-        
         
         }
 }
