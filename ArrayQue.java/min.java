d7a7a6875bb0999596c75729b77cee9df26b17e0
Gourav554 <Jgourav554@gmail.com>
2026-03-04 20:27:57 +0530

    min

--- ArrayQue.java/min.java
+++ ArrayQue.java/min.java
@@ -1,14 +1,16 @@
 import java.util.*;
-public class Q3 {
+public class min {
     public static void main(String[]args){
         int arr[]= {5,2,8,9,65};
         int Min =Integer.MAX_VALUE;
      for(int i=0;i<arr.length;i++){
         if(arr[i]<Min){
             Min=arr[i];
-             System.out.println(Min);
         }
-     }
+    }
+             System.out.println(Min);
+        
+     
     }
     
 }
