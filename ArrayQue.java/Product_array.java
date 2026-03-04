ac990c65dd78bef53890479772450e4672345789
Gourav554 <Jgourav554@gmail.com>
2026-03-04 20:23:09 +0530

    Product

--- ArrayQue.java/Product_array.java
+++ ArrayQue.java/Product_array.java
@@ -12,11 +12,14 @@ public class Product_array {
             nums[i] = sc.nextLong();
         }
         long ans[] = new long[n];
+
         ans[0] = 1;
+
         for(int i = 1; i < n; i++){
             ans[i] = ans[i-1] * nums[i-1];
         }
         long right = 1;
+        
         for(int i = n-1; i >= 0; i--){
             ans[i] = ans[i] * right;
             right = right * nums[i];
