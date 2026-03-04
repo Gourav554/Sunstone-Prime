7d4514be749e3c1f16216df8b4b34dd79468987e
Gourav554 <Jgourav554@gmail.com>
2026-03-04 20:28:59 +0530

    find of an array

--- ArrayQue.java/find.java
+++ ArrayQue.java/find.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Q4 {
+public class find {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
