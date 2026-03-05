32c4518aa340b21bf04bdf708e4c24eac8e202a1
Gourav554 <Jgourav554@gmail.com>
2026-03-05 20:42:14 +0530

    Min

--- Topic.java/Arraylist.java
+++ Topic.java/Arraylist.java
@@ -9,6 +9,7 @@ public class Arraylist {
         list.add(40);
         list.add(50);
          int Max = Collections.max(list);
+         int min = Collections.min(list);
         
         System.out.println(list); //Size
         System.out.println(list.remove(4));  //Remove
