5ee4a91d06c414156052e22a6ab6d562c65af79f
Gourav554 <Jgourav554@gmail.com>
2026-03-13 20:55:03 +0530

    Strings

--- Strings.java
+++ Strings.java
@@ -5,7 +5,7 @@ public class Strings {
          String fullname ="hello bro";
          Scanner sc =new Scanner(System.in);
          String name = sc.next();      // Single word input ke liye without line
-     
+      //  String name = sc.nextLine();      puri line print karwani ho tho with line 
           System.out.println(name); 
           System.out.println(name.length());
 
