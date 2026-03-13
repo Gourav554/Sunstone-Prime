8dae779e30d0942562fe524b5cb7d18d696ec9c9
Gourav554 <Jgourav554@gmail.com>
2026-03-13 20:54:09 +0530

    String

---
+++ Strings.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Strings {
+    public static void main(String[] args) {
+      //  String name = "hello";
+         String fullname ="hello bro";
+         Scanner sc =new Scanner(System.in);
+         String name = sc.next();      // Single word input ke liye without line
+     
+          System.out.println(name); 
+          System.out.println(name.length());
+
+          for (int i=0;i<name.length();i++){
+            System.out.print(name.charAt(i));
+          }
+        }
+      }
