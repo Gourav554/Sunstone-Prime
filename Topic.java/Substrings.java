d7d89873891b48b56b395146b888748660cc738a
Gourav554 <Jgourav554@gmail.com>
2026-03-14 23:52:30 +0530

    substring

---
+++ Topic.java/Substrings.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Substrings {
+    public static void main(String[] args) {
+        
+        String Sentence = "my name is yoyo"; 
+        String name = Sentence.substring(11, Sentence.length());    // Start,end Index      //koi bhi String mmai se sab string nikalne ke liye just a word        
+        System.out.println(name);          
+    }
+}
