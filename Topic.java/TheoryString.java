de23979f3e1dc4ff1b982ce5e35d5e280676ca22
Gourav554 <Jgourav554@gmail.com>
2026-03-15 21:07:22 +0530

    string

---
+++ Topic.java/TheoryString.java
@@ -0,0 +1,15 @@
+public class TheoryString {
+    //       if (name.compareTo(fullname)==0) {
+//           //  S1 > S2  : +ve value dega    Compare to ke cases hai ye ho sakte hai
+//           //  S1 == S2 : 0  dega
+//           //  S1 < S2 : -ve Value dega
+//           //string ka phela character bada hoga uski string vlue jada hogi jab no.of char dono side equal ho tab first char se compare hoga
+//         System.out.println("strings are equal");
+//       }
+//       else{
+//         System.out.println("not equal");
+//       }
+//     } 
+// }
+
+}
