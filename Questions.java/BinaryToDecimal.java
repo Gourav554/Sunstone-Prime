4d6c7c578bffb5a466d30249f46eef4ce3353bab
Gourav554 <Jgourav554@gmail.com>
2026-03-17 20:34:33 +0530

    Binary

--- Questions.java/BinaryToDecimal.java
+++ Questions.java/BinaryToDecimal.java
@@ -1,18 +1,20 @@
 import java.util.*;
+
 public class BinaryToDecimal {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int Sum=0;
-        int pow =0;
-        int g=0;
-        while(n>0){
-            int rem =n%10;
-            Sum = Sum + rem * (int)Math.pow(2, g);
-              g++;
-            n=n/10;
+        
+        int sum = 0;
+        int power = 0;
+
+        while (n > 0) {
+            int rem = n % 10;
+            sum = sum + rem * (int)Math.pow(2, power);
+            power++;
+            n = n / 10;   // ✅ important
         }
-        System.out.println(Sum);
+
+        System.out.println(sum);
     }
-    
 }
