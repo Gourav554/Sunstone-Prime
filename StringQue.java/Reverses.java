5fda52565fb11857db6ce46e043c49aeeccb82d8
Gourav554 <Jgourav554@gmail.com>
2026-03-17 20:24:01 +0530

    String

--- StringQue.java/Reverses.java
+++ StringQue.java/Reverses.java
@@ -5,7 +5,7 @@ public class Reverses {
     String rev ="";
     for(int i=str.length()-1;i>=0;i--){
         rev = rev + str.charAt(i);
-         //System.out.println(i);
+         System.out.println(i);
     }
    System.out.println(rev);
 
