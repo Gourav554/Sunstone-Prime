8a402cf81d79ac65e207e87316027a1fa0358070
Gourav554 <Jgourav554@gmail.com>
2026-03-18 23:37:15 +0530

    Decimal

--- Questions.java/DicemalToBinary.java
+++ Questions.java/DicemalToBinary.java
@@ -3,15 +3,15 @@ public class DicemalToBinary{
    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String binary = sc.nextLine(); // input as string
+        int decimal = sc.nextInt();
 
-        int decimal = Integer.parseInt(binary, 2);
+        String binary = Integer.toBinaryString(decimal);
 
-        System.out.println(decimal);
+        System.out.println(binary);
             
         }
        
     }
     
-    
+
 
