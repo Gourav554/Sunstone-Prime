581036513fdedb208945487935f57d124d5700ed
Gourav554 <Jgourav554@gmail.com>
2026-03-27 21:43:34 +0530

    Sliding

--- ArrayQue.java/Sliding_Window.java
+++ ArrayQue.java/Sliding_Window.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Sliding_Window {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int 
+        int n = sc.nextInt();
         
     }
     
