7c1f1c20a5de3987d32c292438ffb05b1c7ca4a3
Gourav554 <Jgourav554@gmail.com>
2026-03-28 23:27:03 +0530

    Enacpsualtipn'

--- OOPS/Encapsulation.java
+++ OOPS/Encapsulation.java
@@ -1,3 +1,22 @@
-public class Encapsulation {
-    
+
+class Student {
+    private String name;  // data hidden
+
+    // Setter method (data set karne ke liye)
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter method (data get karne ke liye)
+    public String getName() {
+        return name;
+    }
+}
+
+public class Encapsulation{
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Rahul");        // set value
+        System.out.println(s.getName()); // get value
+    }
 }
