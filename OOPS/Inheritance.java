0b0bab3f97874ffc47ea512e2a08a81e3b83876d
Gourav554 <Jgourav554@gmail.com>
2026-03-28 22:29:25 +0530

    Inheritance

--- OOPS/Inheritance.java
+++ OOPS/Inheritance.java
@@ -1,3 +1,19 @@
-public class Inheritance {
-    
+
+class Animal {
+    void eat() {
+        System.out.println("Eating...");
+    }
 }
+class Dog extends Animal{
+    void bark() {
+        System.out.println("Barking...");
+    }
+}
+public class Inheritance {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat(); // Inherited method
+        dog.bark(); // Own method
+    }
+}
+   
