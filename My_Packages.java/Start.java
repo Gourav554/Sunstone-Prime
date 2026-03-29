9a97ae44421cb3301b3d14bc2a75667fb5b1515d
Gourav554 <Jgourav554@gmail.com>
2026-03-29 13:55:30 +0530

    pack

---
+++ My_Packages.java/Start.java
@@ -0,0 +1,10 @@
+package My_Packages;
+public class Start {
+    public void display() {
+        System.out.println("Welcome to the Start class in the Packages package!");
+    }
+    public static void main(String[] args) {
+        Start start = new Start();
+        start.display();
+    }
+}
