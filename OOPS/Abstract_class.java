bb1c6bfa257f5716a7cc83999cb1177aa5e39ef8
Gourav554 <Jgourav554@gmail.com>
2026-03-29 12:09:08 +0530

    Abstarct

---
+++ OOPS/Abstract_class.java
@@ -0,0 +1,25 @@
+abstract class Animal {
+    abstract void sound(); // abstract method
+
+    void sleep() {
+        System.out.println("Sleeping...");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Barking");
+    }
+}
+
+public class Abstract_class {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();  // Barking
+        d.sleep();  // Sleeping...
+    }
+}
+
+//    Animal ek abstract class hai
+//    sound() ka implementation hidden hai
+//    Dog usko define karta hai
