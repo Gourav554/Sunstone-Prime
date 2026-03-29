cbe71d17b80bd572ca814639f92d6c61bc7b82b8
Gourav554 <Jgourav554@gmail.com>
2026-03-29 12:15:12 +0530

    Interface

--- OOPS/Abstract_Interface.java
+++ OOPS/Abstract_Interface.java
@@ -7,3 +7,9 @@ class Car implements Vehicle {
         System.out.println("Car starts with key");
     }
 }
+public class Abstract_Interface{
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.start();  // Car starts with key
+    }
+}
