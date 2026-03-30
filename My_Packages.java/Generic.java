664287311a36522bc5719c58738dccc188147673
Gourav554 <Jgourav554@gmail.com>
2026-03-30 20:02:22 +0530

    Generic

--- My_Packages.java/Generic.java
+++ My_Packages.java/Generic.java
@@ -5,4 +5,13 @@ public class Generic {
     //Object class is top most Parent class of all java classes, isliye Object data type se hum kisi bhi type ka data store kar sakte hain, lekin type safety nahi hoti
     Object container;  // Object data type se hum kisi bhi type ka data store kar sakte hain, lekin type safety nahi hoti
 
+public void box(Object item) {
+    this.container = item;
+}
+
+public Object getValue(){
+    return this.container;
+}
+
+
 }
