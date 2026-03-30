c1f8f4ffcce1c1f3afa526a41009a2384806f3b2
Gourav554 <Jgourav554@gmail.com>
2026-03-30 19:44:00 +0530

    Generic

--- My_Packages.java/Generic.java
+++ My_Packages.java/Generic.java
@@ -4,14 +4,16 @@ public class Generic {
         // Generic programming ka concept hai jisme hum data types ke saath 
         // flexible code likh sakte hain. Isme hum type parameters ka use karte hain.
        //<_____> iske bEECH MAI JO BHI LIKHENGE USSI TYPE KA DATA STORE HOGA 
-  list<String> list = new Arraylist<>();
+  ArrayList<String> list = new ArrayList<>();
   list.add("Gourav");
 
 
 
   // Another Liklne se hum ek aur list bana sakte hain jisme hum alag type ka data store kar sakte hain,
-  list anotherlist = new Arraylist();
+  ArrayList anotherlist = new ArrayList<>();
   anotherlist.add("Jaiswal");
+  anotherlist.add(20);
+  anotherlist.add(132.54);
 
   System.out.println(list);
   System.out.println(anotherlist);
