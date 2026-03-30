67995f6f223c41e03ff0d737fff5979e91439c60
Gourav554 <Jgourav554@gmail.com>
2026-03-30 19:41:12 +0530

    GFEneric

---
+++ My_Packages.java/Generic.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class Generic {
+    public static void main(String[] args) {
+        // Generic programming ka concept hai jisme hum data types ke saath 
+        // flexible code likh sakte hain. Isme hum type parameters ka use karte hain.
+       //<_____> iske bEECH MAI JO BHI LIKHENGE USSI TYPE KA DATA STORE HOGA 
+  list<String> list = new Arraylist<>();
+  list.add("Gourav");
+
+
+
+  // Another Liklne se 
+  list anotherlist = new Arraylist();
+  anotherlist.add("Jaiswal");
+
+  System.out.println(list);
+  System.out.println(anotherlist);
+    }
+}
