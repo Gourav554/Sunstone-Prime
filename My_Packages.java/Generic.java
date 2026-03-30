39018b40d4139ef1765fc3790e6222d910c0b8eb
Gourav554 <Jgourav554@gmail.com>
2026-03-30 20:00:39 +0530

    Generic

---
+++ My_Packages.java/Generic.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Generic {
+    // String,water container;   Only String data store kar sakte hain
+
+    //Object class is top most Parent class of all java classes, isliye Object data type se hum kisi bhi type ka data store kar sakte hain, lekin type safety nahi hoti
+    Object container;  // Object data type se hum kisi bhi type ka data store kar sakte hain, lekin type safety nahi hoti
+
+}
