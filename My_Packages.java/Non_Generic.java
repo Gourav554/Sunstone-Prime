6b5c7c63313c206c43d2901d6da1335671344612
Gourav554 <Jgourav554@gmail.com>
2026-03-30 19:56:48 +0530

    Non

--- My_Packages.java/Non_Generic.java
+++ My_Packages.java/Non_Generic.java
@@ -1,12 +1,11 @@
 import java.util.ArrayList;
-public class Generic {
+public class Non_Generic {
     public static void main(String[] args) {
         // Generic programming ka concept hai jisme hum data types ke saath 
         // flexible code likh sakte hain. Isme hum type parameters ka use karte hain.
        //<_____> iske bEECH MAI JO BHI LIKHENGE USSI TYPE KA DATA STORE HOGA 
   ArrayList<String> list = new ArrayList<>();
   list.add("Gourav");
-
 
 
   // Another Liklne se hum ek aur list bana sakte hain jisme hum alag type ka data store kar sakte hain,
