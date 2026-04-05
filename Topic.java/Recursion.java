661c53dfc14ff2fb3098349c382d187bdf961fc2
Gourav554 <Jgourav554@gmail.com>
2026-04-05 18:21:00 +0530

    recursion

--- Topic.java/Recursion.java
+++ Topic.java/Recursion.java
@@ -2,8 +2,6 @@ public class Recursion {
     public static void main(String[] args) {
         
         // Factorial of a number using recursion
-        int n = 5;
-        int result = factorial(n);
-        System.out.println("Factorial of " + n + " is: " + result);
+       
     }
 }
